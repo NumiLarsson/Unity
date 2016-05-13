69faea53e0d8742d90dd1824cb672197c7ac7669
NumiLarsson <ante9311@gmail.com>
2016-05-13 14:44:44 +0200

    FUCK UNITY

---
+++ src/UnityClient/Asteroids/Assets/Asteroid/Create.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Create : MonoBehaviour {
+
+	public GameObject prefab;
+	GameObject prefabClone;
+
+	void Update() {
+		if (Input.GetKeyDown ("r")) {
+			prefabClone = Instantiate (prefab, transform.position, Quaternion.identity) as GameObject;
+			Destroy (prefabClone, 3); //Destroys object after 3 seconds
+		}
+	}
+}
---
+++ src/UnityClient/Asteroids/Assets/Asteroid/OutsideBounds.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutsideBounds : MonoBehaviour {
+
+	public float MinX;
+	public float MaxX;
+	public float MinY;
+	public float MaxY;
+
+
+	void FixedUpdate() {
+		float x = transform.position.x;
+		float y = transform.position.y;
+
+		if (x < MinX) x = MaxX;
+		else if (x > MaxX) x = MinX;
+
+		if (y < MinY)y = MaxY;
+		else if (y > MaxY)y = MinY;
+
+		transform.position = new Vector3 (x, y, transform.position.z);
+
+		}
+	}
