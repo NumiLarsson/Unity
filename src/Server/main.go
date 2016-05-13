bed4aaec2441785095a33368bf1e1beecd58b2ba
NumiLarsson <ante9311@gmail.com>
2016-05-13 14:25:38 +0200

    No longer blocking in listen()

--- src/Server/main.go
+++ src/Server/main.go
@@ -1,10 +1,11 @@
 package main
 
-import "./asteroids"
+//import "./asteroids"
+import "github.com/numilarsson/ospp-2016-group-08/src/server/asteroids"
 
 func main() {
 
 	var server = asteroids.CreateServer()
-	server.Listen(server.CreateFakeUser())
+	server.Listen(make(chan asteroids.Data)/*server.CreateFakeUser()*/)
 
 }
