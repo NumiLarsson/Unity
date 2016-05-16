6b16dc224754f9b2d5313c246c77782f52405c1c
Marcus Andersson <marcus@marcus-andersson.com>
2016-05-16 15:20:24 +0200

    Adds a debug print interface and some more tests

--- src/Server/asteroids/asteroid_test.go
+++ src/Server/asteroids/asteroid_test.go
@@ -3,11 +3,11 @@ package asteroids
 import "testing"
 
 //Movement a asteroid three times in a pase of xStep and yStep
-func Movement(xStep int, yStep int) *asteroid {
+func Movement(xStep int, yStep int) *Asteroid {
 
 	a := newAsteroid()
-	a.x = 5
-	a.y = 5
+	a.X = 5
+	a.Y = 5
 
 	a.xStep = xStep
 	a.yStep = yStep
@@ -24,14 +24,14 @@ func TestMovementHorisontal(t *testing.T) {
 
 	var roid1 = Movement(1, 0)
 
-	if roid1.x != 8 || roid1.y != 5 {
-		t.Error("Expected position (x,y) = (8,5), got (", roid1.x, ",", roid1.y, ")")
+	if roid1.X != 8 || roid1.Y != 5 {
+		t.Error("Expected position (x,y) = (8,5), got (", roid1.X, ",", roid1.Y, ")")
 	}
 
 	var roid2 = Movement(-1, 0)
 
-	if roid2.x != 2 || roid2.y != 5 {
-		t.Error("Expected position (x,y) = (2,5), got (", roid2.x, ",", roid2.y, ")")
+	if roid2.X != 2 || roid2.Y != 5 {
+		t.Error("Expected position (x,y) = (2,5), got (", roid2.X, ",", roid2.Y, ")")
 	}
 }
 
@@ -40,14 +40,14 @@ func TestMovementHorisontal(t *testing.T) {
 func TestMovementVertical(t *testing.T) {
 	var roid1 = Movement(0, 1)
 
-	if roid1.x != 5 || roid1.y != 8 {
-		t.Error("Expected position (x,y) = (5,8), got (", roid1.x, ",", roid1.y, ")")
+	if roid1.X != 5 || roid1.Y != 8 {
+		t.Error("Expected position (x,y) = (5,8), got (", roid1.X, ",", roid1.Y, ")")
 	}
 
 	var roid2 = Movement(0, -1)
 
-	if roid2.x != 5 || roid2.y != 2 {
-		t.Error("Expected position (x,y) = (5,2), got (", roid2.x, ",", roid2.y, ")")
+	if roid2.X != 5 || roid2.Y != 2 {
+		t.Error("Expected position (x,y) = (5,2), got (", roid2.X, ",", roid2.Y, ")")
 	}
 }
 
@@ -55,13 +55,13 @@ func TestMovementVertical(t *testing.T) {
 func TestMovementDiagonal(t *testing.T) {
 	var roid1 = Movement(1, 1)
 
-	if roid1.x != 8 || roid1.y != 8 {
-		t.Error("Expected position (x,y) = (8,8), got (", roid1.x, ",", roid1.y, ")")
+	if roid1.X != 8 || roid1.Y != 8 {
+		t.Error("Expected position (x,y) = (8,8), got (", roid1.X, ",", roid1.Y, ")")
 	}
 
 	var roid2 = Movement(-1, -1)
 
-	if roid2.x != 2 || roid2.y != 2 {
-		t.Error("Expected position (x,y) = (2,2), got (", roid2.x, ",", roid2.y, ")")
+	if roid2.X != 2 || roid2.Y != 2 {
+		t.Error("Expected position (x,y) = (2,2), got (", roid2.X, ",", roid2.Y, ")")
 	}
 }
