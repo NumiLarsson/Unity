f5042fc43eb9c7d2d282fc9936ca9da672cc2bee
peberglund <peterberglund90@gmail.com>
2016-05-18 11:31:04 +0200

    added test of inbound function

--- src/Server/asteroids/asteroid_test.go
+++ src/Server/asteroids/asteroid_test.go
@@ -19,6 +19,15 @@ func Movement(xStep int, yStep int) *Asteroid {
 	return a
 }
 
+func CreateFakeAsteroid(x int, y int) *Asteroid {
+
+	a := newAsteroid()
+	a.X = x
+	a.Y = y
+
+	return a
+}
+
 // TestMovementHorisontal tests to move a asteroid Horisontal
 func TestMovementHorisontal(t *testing.T) {
 
@@ -67,9 +76,44 @@ func TestMovementDiagonal(t *testing.T) {
 }
 
 //TestInBounds tests if an asteroid is inside the gameboard
-func TestInBounds(t *testing.T){
-	a := newAsteroid()
-	manager = newAsteroidManager()
-	manager.xMax = 10
-	manager.yMax = 10	
+func TestInBounds(t *testing.T) {
+
+	var manager = newAsteroidManager()
+	manager.xMax = 100
+	manager.yMax = 100
+
+	var a1 = CreateFakeAsteroid(-1, (manager.yMax + 1))
+	var a2 = CreateFakeAsteroid(-1, (manager.yMax - 1))
+	var a3 = CreateFakeAsteroid(10, (manager.yMax + 100))
+
+	if a1.inBounds(manager) {
+		t.Error("Error in X or Y-coordinate when Y > yMax and X < 0 ")
+	}
+
+	if a2.inBounds(manager) {
+		t.Error("Error in X when X < 0  ")
+	}
+
+	if a3.inBounds(manager) {
+		t.Error("Error in Y when Y > yMax ")
+	}
+
+	for i := 0; i < manager.xMax; i++ {
+
+		var a = CreateFakeAsteroid(i, 0)
+
+		if !a.inBounds(manager) {
+			t.Error("Error when asteroid is inbound on coordinate: (", a.X, ",", a.Y, ")")
+		}
+	}
+
+	for i := 0; i < manager.yMax; i++ {
+
+		var a = CreateFakeAsteroid(0, i)
+
+		if !a.inBounds(manager) {
+			t.Error("Error when asteroid is inbound on coordinate: (", a.X, ",", a.Y, ")")
+		}
+	}
+
 }
