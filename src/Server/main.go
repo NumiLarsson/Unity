5a4c92ffd321f9cf51983df45a4868920c4287e1
Marcus Andersson <marcus@marcus-andersson.com>
2016-05-19 14:23:33 +0200

    Adds a fake mode

--- src/Server/main.go
+++ src/Server/main.go
@@ -11,14 +11,28 @@ func main() {
 
 	argsWithoutProg := os.Args[1:]
 	var inDebugMode = true
+	var inFakeMode = false
+
+	fmt.Println("\nWelcome to the back-end of Asteroids 1.0")
+	fmt.Println("Server starting…")
+	fmt.Print("Parameters: ")
 
 	if len(argsWithoutProg) > 0 && argsWithoutProg[0] == "-s" {
 		inDebugMode = false
-		fmt.Println("\nServer starting in silent mode\n======")
+		fmt.Print("Default, silent\n\n")
+	}
+
+	if len(argsWithoutProg) > 0 && argsWithoutProg[0] == "-f" {
+		inFakeMode = true
+		fmt.Print("Fake users, verbose\n\n")
 	}
 
 	var server = asteroids.CreateServer(inDebugMode)
-	//server.Listen(make(chan asteroids.Data) /*server.CreateFakeUser()*/)
-	server.Listen(server.CreateFakeUser())
+
+	if inFakeMode == true {
+		server.Listen(server.CreateFakeUser())
+	} else {
+		server.Listen(make(chan asteroids.Data))
+	}
 
 }
