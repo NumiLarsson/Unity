6614e86c0dedc42f62118340eda0da394b8d479d
Marcus Andersson <marcus@marcus-andersson.com>
2016-05-20 20:16:05 +0200

    Cleaner main function

--- src/Server/main.go
+++ src/Server/main.go
@@ -7,27 +7,18 @@ import (
 	"./asteroids"
 )
 
+const (
+	serverInacitivityLimitReal = 60
+	serverInacitivityLimitFake = 5
+)
+
 func main() {
 
-	argsWithoutProg := os.Args[1:]
-	inactivityLimit := 60
-	inDebugMode := true
-	inFakeMode := false
+	greetUser()
 
-	fmt.Println("\nWelcome to the back-end of Asteroids 1.0")
-	fmt.Println("Server starting…")
-	fmt.Print("Parameters: ")
-
-	if len(argsWithoutProg) > 0 && argsWithoutProg[0] == "-s" {
-		inDebugMode = false
-		fmt.Print("Default, silent\n\n")
-	}
-
-	if len(argsWithoutProg) > 0 && argsWithoutProg[0] == "-f" {
-		inFakeMode = true
-		inactivityLimit = 5
-		fmt.Print("Fake users, verbose\n\n")
-	}
+	inDebugMode := setDebugMode()
+	inFakeMode := setFakeMode()
+	inactivityLimit := setInactivityLimit(inFakeMode)
 
 	var server = asteroids.CreateServer(inDebugMode, inactivityLimit)
 
@@ -38,5 +29,45 @@ func main() {
 	}
 
 	asteroids.DebugPrint(fmt.Sprintln("[MAIN] Dead"))
+
+}
+
+func greetUser() {
+	fmt.Println("\nWelcome to the back-end of Asteroids 1.0")
+	fmt.Println("Server starting…")
+	fmt.Print("Parameters: ")
+}
+
+func setDebugMode() bool {
+
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) > 0 && argsWithoutProg[0] == "-s" {
+		fmt.Print("Default, silent\n\n")
+		return false
+	}
+
+	return true
+
+}
+
+func setFakeMode() bool {
+
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) > 0 && argsWithoutProg[0] == "-f" {
+		fmt.Print("Fake users, verbose\n\n")
+		return true
+	}
+
+	return false
+
+}
+
+func setInactivityLimit(inFakeMode bool) int {
+
+	if !inFakeMode {
+		return serverInacitivityLimitReal
+	}
+
+	return serverInacitivityLimitFake
 
 }
